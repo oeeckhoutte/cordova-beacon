4a2ab4e267321e3bd5a1811bbd5cb7dcbff718ee
Jarno van Wezel <jarnovwezel@me.com>
2014-01-30 15:30:10 +0100

    Update BluetoothPlugin.java

--- src/android/BluetoothPlugin.java
+++ src/android/BluetoothPlugin.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 
-//import org.apache.cordova.CordovaWebView;
-//import org.apache.cordova.PluginResult;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-//import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaInterface;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
